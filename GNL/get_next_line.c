6ef7a06379262c64a066810a18bfcf7c7313cf6e
Knulpinette <ophelie.surcouf@gmail.com>
2021-03-15 14:34:14 +0000

    added comments on get_nex_line.C

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -10,13 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** 🦕
+** function : get_next_line
+** This function reads the next line in a file descriptor and saves it.
+** Return 1 if read the line.
+** Return 0 if reached EOF.
+** Return -1 if error.
+** Can handle multiple file descriptors at once.
+** 🦕
+*/
+
 #include "get_next_line.h"
+
+/*
+** function : strdel
+** Frees the memory allocated on a given string, then set it's value to 0.
+** Important in the case of a static variable !
+*/
 
 static void	ft_strdel(char **saved)
 {
 	free(saved[0]);
 	saved[0] = NULL;
 }
+
+/*
+** function : fill_static
+** This function fills up the static char* with:
+** 1. The buffer read if it's empty.
+** 2. The previous value it contains + the new buffer read if not.
+*/
 
 static void	fill_static(char *buf, char **saved)
 {
@@ -31,6 +55,16 @@ static void	fill_static(char *buf, char **saved)
 		*saved = temp;
 	}
 }
+
+/*
+** function : create_line
+** This function saves the *line and returns the int value of get_next_line.
+** 1. We save the given buffer until the first newline into *line.
+**    We jump to else and save the reminder of that line into the static.
+**    If what's to be saved is '/0' => we free the static char*
+** 2. If we have reached EOF and there's no new line, we save the static
+**    into line, free the static, return 0.
+*/
 
 static int	create_line(char **line, char **saved)
 {
@@ -58,6 +92,21 @@ static int	create_line(char **line, char **saved)
 	}
 	return (1);
 }
+
+/*
+** function : get_next_line core
+** This function executes get_next_line.
+** 1. We check for various errors.
+** 2. We allocate the buffer memory and fill it up in a loop until the
+**    buffer ever contains a newline.
+**    We free the buffer to avoid leaks.
+** 2. If everything is empty, we've reached EOF, so we create a
+**    a freeable string for *line.
+** 3. We return create_line.
+** Here we use an Buffer Array to handle multiple file descriptors at
+** once. The maximum number you can get on Linus is 1024 but another
+** solution could be to use the MAX_FD <limits.h> variable. Or lists.
+*/
 
 int			get_next_line(int fd, char **line)
 {
