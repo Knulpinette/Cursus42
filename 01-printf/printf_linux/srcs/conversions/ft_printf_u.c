4c370e44d27e3d8480e76886dfcd5f84664fbb9f
Ophelie Surcouf <osurcouf@c2r2s4.42lisboa.com>
2021-08-04 13:54:28 +0100

    rename of directories

---
+++ 01-printf/printf_linux/srcs/conversions/ft_printf_u.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_u.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osurcouf <.@student.42lisboa.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/12 13:34:00 by osurcouf          #+#    #+#             */
+/*   Updated: 2021/03/12 13:34:02 by osurcouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/ft_printf.h"
+
+/*
+** 🦕
+** function : convert_u
+** return : the given unsigned int converted into a string.
+** 🦕
+*/
+
+/*
+** function : unsigned_itoa
+** Works exactly like the classic version of ft_itoa, but
+** for an unsigned int !
+*/
+
+static char	*ft_itoa_u(unsigned int n)
+{
+	unsigned int		size;
+	unsigned long int	nb;
+	char				*str;
+
+	nb = (long int)n;
+	size = 2;
+	while ((n / 10) != 0)
+	{
+		size++;
+		n = n / 10;
+	}
+	str = (char *)ft_calloc(sizeof(char), size);
+	if (!(str))
+		return (NULL);
+	str[size - 1] = '\0';
+	while (--size > 0)
+	{
+		str[size - 1] = '0' + nb % 10;
+		nb = nb / 10;
+	}
+	return (str);
+}
+
+/*
+** function : handle_width_and_precision
+** Modifies the values directly in the other function
+** -> that's why it deals with **pointers.
+** Special case : if there is width AND precision, the function will
+** 				  ignore any flag '0'.
+*/
+
+static void	handle_w_and_p(char **conv, t_flags **flags, int *len)
+{
+	if ((flags[0]->precision + 1) > *len)
+	{
+		*conv = align_nb_precision(flags[0]->precision, *conv, *len);
+		*len = (int)ft_strlen(*conv);
+	}
+	if (flags[0]->width > *len)
+	{
+		if (flags[0]->precision > 0 || (flags[0]->dot && !flags[0]->precision))
+			flags[0]->zero = 0;
+		*conv = define_align_width(*conv, flags[0], *len);
+	}
+}
+
+/*
+** function : convert_u
+** return : the given unsigned int into a string.
+** 1. Converts the given unsigned int into a string.
+** 2. Sends the data to the width and precision function.
+** Special case : If the given int is 0, + precision is '.' or '.0'
+**     			  AND there is width, the function returns a string '\0'.
+**				  Else it creates a freeable empty string.
+*/
+
+char	*convert_u(va_list arg, t_flags *flags)
+{
+	char			*conv;
+	unsigned int	i;
+	int				len;
+
+	i = va_arg(arg, unsigned int);
+	conv = ft_itoa_u(i);
+	if (!conv)
+		return (NULL);
+	len = (int)ft_strlen(conv);
+	if (flags->dot && !flags->precision && conv[0] == '0')
+	{
+		free(conv);
+		if (!flags->width)
+			return (ft_strdup("\0"));
+		conv = ft_strdup(" ");
+	}
+	handle_w_and_p(&conv, &flags, &len);
+	return (conv);
+}
---
+++ 01-printf/printf_linux/srcs/print/ft_print_arg.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_arg.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osurcouf <.@student.42lisboa.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/20 19:32:58 by osurcouf          #+#    #+#             */
+/*   Updated: 2021/03/20 19:32:59 by osurcouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/ft_printf.h"
+
+/*
+** 🦕
+** function : printing_strings
+** return : number of character printed
+** This function prints the argument converted into a string and frees it.
+** 🦕
+*/
+
+int	ft_print_arg(char *print)
+{
+	char	*end_print;
+	int		count;
+
+	end_print = print;
+	while (*end_print)
+		end_print++;
+	write(1, print, (end_print - print));
+	count = end_print - print;
+	free(print);
+	return (count);
+}
+
+/*
+** function : print_case_c_if_null
+** return : number of character printed
+** This function prints the argument converted into a string and frees it.
+** It handles the special case of the c conversion where the given character
+** is '\0'. It prints it separately and includes it in the count of printed
+** characters even though it is not visible in the result.
+** The variable align is set in the conversion function and indicates that
+** there is a \0 aligned to the left, but also other empty characters to the
+** right.
+*/
+
+int	handle_align(int *align, char null, char *print, char *end_print)
+{
+	int	i;
+
+	i = 0;
+	if (*align == left)
+	{
+		write(1, &null, 1);
+		i++;
+		while (end_print[i])
+			i++;
+		write(1, (print + 1), i - 1);
+	}
+	if (*align == right)
+	{
+		while (end_print[i])
+			i++;
+		write(1, print, i);
+		write(1, &null, 1);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_print_c_null(char *print, int *align)
+{
+	char	*end_print;
+	int		count;
+	char	null;
+
+	null = '\0';
+	count = 0;
+	end_print = print;
+	if (!align)
+	{
+		write(1, &null, 1);
+		return (1);
+	}
+	else
+		count = handle_align(align, null, print, end_print);
+	free(print);
+	return (count);
+}
+
+/*
+** function : check_length_of_arg
+** return : number of characters from % to i_am_conversion
+** This function allows to set the cursor in the string to the right place
+** for the next loop once it read an argument.
+** Len is set to 1 to count the % character in the total since *(s+1) is sent
+** from ft_printf.
+*/
+
+int	ft_check_len_arg(const char *s)
+{
+	int	len;
+
+	len = 1;
+	while (*s)
+	{
+		if (ft_i_am_conversion(*s))
+			return (len);
+		s++;
+		len++;
+	}
+	return (0);
+}
